42f20c36fd3bb35e61fda5d6e3882c0f87b79b30
ival06001 <ival06001@E-R90YE0B5>
2023-09-15 10:32:06 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //frågar efter bok
+            Console.WriteLine("Vad har du för favoritbok?");
+            string book = Console.ReadLine();
+
+            //Skriver ut boken
+            Console.WriteLine($"Din favoritbok heter {book}.");
+          
+            Console.ReadKey();
+        }
+    }
+}
